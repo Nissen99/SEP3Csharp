18681f69b1984d01f1bfd3eb48e0e990541cf757
Mikkel <Mikkelnj411@gmail.com>
2021-11-24 16:06:20 +0100

    Start

---
+++ SEP3CsharpGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SEP3CsharpGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
