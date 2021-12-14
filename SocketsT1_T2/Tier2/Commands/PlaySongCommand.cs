6fc3389746bc811ac0d8b9b285af1376d944dc2b
Bimmerlynge <82088422+Bimmerlynge@users.noreply.github.com>
2021-12-14 12:37:34 +0100

    Update PlaySongCommand.cs

--- SocketsT1_T2/Tier2/Commands/PlaySongCommand.cs
+++ SocketsT1_T2/Tier2/Commands/PlaySongCommand.cs
@@ -12,7 +12,7 @@ using SocketsT1_T2.Tier2.Util;
 
 /*
  * Commando klasse. Den klasse styrer udpakningen af handlingen 'Play Song'.
- * Den sender det udpakkede objekt til sin receiver IPlayService og modtager et byte array.
+ * Den sender det udpakkede objekt til sin receiver IPlayService og returnerer en respons.
  */
 
 namespace SocketsT1_T2.Tier2.Commands
