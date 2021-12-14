711fdeb7b1773c268c3865b9f2050ee2584da6bf
Bimmerlynge <82088422+Bimmerlynge@users.noreply.github.com>
2021-12-14 12:36:40 +0100

    Update GetPlaylistsCommand.cs

--- SocketsT1_T2/Tier2/Commands/GetPlaylistsCommand.cs
+++ SocketsT1_T2/Tier2/Commands/GetPlaylistsCommand.cs
@@ -10,7 +10,7 @@ using SocketsT1_T2.Tier2.Util;
 
 /*
  * Commando klasse. Den klasse styrer udpakningen af handlingen 'Get Playlists'.
- * Den sender det udpakkede objekt til sin receiver IPlaylistService.
+ * Den sender det udpakkede objekt til sin receiver IPlaylistService og returnerer en respons.
  */
 
 namespace SocketsT1_T2.Tier2.Commands
