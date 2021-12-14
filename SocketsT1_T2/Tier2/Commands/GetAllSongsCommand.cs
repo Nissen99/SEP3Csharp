80cba1f81652056d44eb09b8cf0ab1190328cef1
Bimmerlynge <82088422+Bimmerlynge@users.noreply.github.com>
2021-12-14 12:35:32 +0100

    Update GetAllSongsCommand.cs

--- SocketsT1_T2/Tier2/Commands/GetAllSongsCommand.cs
+++ SocketsT1_T2/Tier2/Commands/GetAllSongsCommand.cs
@@ -14,7 +14,7 @@ using SocketsT1_T2.Tier2.Util;
 
 /*
  * Commando klasse. Den klasse styrer udpakningen af handlingen 'Get All Songs'.
- * Denne handling har ingen objekter den sender videre til sin receiver ILibraryService
+ * Denne handling har ingen objekter den sender videre til sin receiver ILibraryService, den returnerer en respons.
  */
 
 namespace SocketsT1_T2.Tier2.Commands
