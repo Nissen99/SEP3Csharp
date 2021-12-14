175f324d929ceadf3bfebd4eaf6063ebe6195705
Bimmerlynge <82088422+Bimmerlynge@users.noreply.github.com>
2021-12-14 12:12:20 +0100

    Update GetAllAlbumsCommand.cs

--- SocketsT1_T2/Tier2/Commands/GetAllAlbumsCommand.cs
+++ SocketsT1_T2/Tier2/Commands/GetAllAlbumsCommand.cs
@@ -8,6 +8,11 @@ using Factory;
 using SocketsT1_T2.Shared;
 using SocketsT1_T2.Tier2.Util;
 
+/*
+ * Commando klasse. Den klasse styrer udpakningen af handlingen 'Get All Albums'.
+ * Den sender de udpakkede objekter til sin receiver IAlbumService.
+ */
+
 namespace SocketsT1_T2.Tier2.Commands
 {
     public class GetAllAlbumsCommand : ICommand
