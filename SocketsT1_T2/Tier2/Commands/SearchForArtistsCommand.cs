1c7e32e55b0429b81c1e7bb54bcbf4387de60ebf
Bimmerlynge <82088422+Bimmerlynge@users.noreply.github.com>
2021-12-14 12:39:34 +0100

    Update SearchForArtistsCommand.cs

--- SocketsT1_T2/Tier2/Commands/SearchForArtistsCommand.cs
+++ SocketsT1_T2/Tier2/Commands/SearchForArtistsCommand.cs
@@ -12,7 +12,7 @@ using SocketsT1_T2.Tier2.Util;
 
 /*
  * Commando klasse. Den klasse styrer udpakningen af handlingen 'Search For Artists'.
- * Den sender det udpakkede objekt til sin receiver IArtistService og returnerer en Server Response.
+ * Den sender det udpakkede objekt til sin receiver IArtistService og returnerer en respons.
  */
 
 
