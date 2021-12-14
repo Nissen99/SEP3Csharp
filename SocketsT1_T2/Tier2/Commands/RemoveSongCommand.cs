abc03b7238065ea4eea3bb9ea319301a68e7a1ca
Bimmerlynge <82088422+Bimmerlynge@users.noreply.github.com>
2021-12-14 12:38:22 +0100

    Update RemoveSongCommand.cs

--- SocketsT1_T2/Tier2/Commands/RemoveSongCommand.cs
+++ SocketsT1_T2/Tier2/Commands/RemoveSongCommand.cs
@@ -11,7 +11,7 @@ using SocketsT1_T2.Tier2.Util;
 
 /*
  * Commando klasse. Den klasse styrer udpakningen af handlingen 'Remove Song'.
- * Den sender det udpakkede objekt til sin receiver ISongManageService og returnerer et tomt TransferObj.
+ * Den sender det udpakkede objekt til sin receiver ISongManageService og returnerer en respons.
  */
 
 namespace SocketsT1_T2.Tier2.Commands
