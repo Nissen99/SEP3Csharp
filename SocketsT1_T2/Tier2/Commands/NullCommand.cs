5f7873a1c32c0b24301f1943ac4ad77b3563975f
Bimmerlynge <82088422+Bimmerlynge@users.noreply.github.com>
2021-12-14 12:24:33 +0100

    Update NullCommand.cs

--- SocketsT1_T2/Tier2/Commands/NullCommand.cs
+++ SocketsT1_T2/Tier2/Commands/NullCommand.cs
@@ -3,6 +3,10 @@ using System.Threading.Tasks;
 using SocketsT1_T2.Shared;
 using SocketsT1_T2.Tier2.Util;
 
+/*
+ * Commando klasse. Den klasse bruges til tilfældet at et kommando kald ikke findes. Uden denne klasse vil man
+ * ende med en NullPointer exception.
+ */
 namespace SocketsT1_T2.Tier2.Commands
 {
     public class NullCommand : ICommand
