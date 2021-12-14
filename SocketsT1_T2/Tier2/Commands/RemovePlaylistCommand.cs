6643338ecacd2da6c68be753d8cd0dabee6c7039
Bimmerlynge <82088422+Bimmerlynge@users.noreply.github.com>
2021-12-14 12:38:08 +0100

    Update RemovePlaylistCommand.cs

--- SocketsT1_T2/Tier2/Commands/RemovePlaylistCommand.cs
+++ SocketsT1_T2/Tier2/Commands/RemovePlaylistCommand.cs
@@ -12,7 +12,7 @@ using SocketsT1_T2.Tier2.Util;
 
 /*
  * Commando klasse. Den klasse styrer udpakningen af handlingen 'Remove Playlist'.
- * Den sender det udpakkede objekt til sin receiver IPlaylistService og returnerer et tomt TransferObj.
+ * Den sender det udpakkede objekt til sin receiver IPlaylistService og returnerer en respons.
  */
 
 namespace SocketsT1_T2.Tier2.Commands
